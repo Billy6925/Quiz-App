5a14383af8340c6cc9528243ec21d9d908ea3330
Natasha Nduku <natashanduku0@gmail.com>
2025-01-29 11:55:08 +0300

    adds placeholders for categories

--- src/components/CategorySelector.jsx
+++ src/components/CategorySelector.jsx
@@ -2,6 +2,11 @@ export default function CategorySelector() {
     return (
       <div>
         <h2>Select a Category</h2>
+        <div>
+          <button>Category 1</button>
+          <button>Category 2</button>
+          <button>Category 3</button>
+        </div>
       </div>
     );
   }
