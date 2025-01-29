7ff4c541992d51ace6b946535cb9d345ba62ec82
Natasha Nduku <natashanduku0@gmail.com>
2025-01-29 11:24:13 +0300

    adds list items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 export default function Navbar() {
     return(
         <nav>
-          {   }
+          <ul>
+           <li></li>
+           <li></li>
+           <li></li>
+           <li></li>  
+          </ul>
         </nav>
       );
   }
