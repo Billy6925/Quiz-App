958e35ef6cb1bcb64fd9fe5100396adb50ed1590
MELODY SIMWA <simwamelody@gmail.com>
2025-01-29 14:29:51 +0300

    update App.js

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -7,8 +7,9 @@ const QuizQuestion=()=>{
     //state management
 const [questions, setQuestions]=useState([]);
 const [currentIndex, setCurrentIndex] = useState(0);
+const [loading, setLoading] = useState(true);
 const [completed, setCompleted] = useState(false);
-}
+
 
  // Fetch questions from API
  useEffect(() => {
@@ -43,12 +44,42 @@ const [completed, setCompleted] = useState(false);
       );
     }
   }, [completed]);
+// Calculate progesss
+const progress = questions.length ? ((currentIndex + 1) / questions.length) * 100 : 0;
+
+// Handlers
+const handleNext = () => {
+  if (currentIndex < questions.length - 1) {
+    setCurrentIndex(currentIndex + 1);
+  } else {
+    setCompleted(true);
+    alert("Quiz Completed! Data saved to localStorage.");
+  }
+};
+
+const handlePrevious = () => {
+  if (currentIndex > 0) {
+    setCurrentIndex(currentIndex - 1);
+  }
+};
+
+  // Loading State
+  if (loading) {
+    return <div>Loading questions...</div>;
+  }
+
+  // Error State
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 return(
     <div>
+{/*progress indicator*/}
     <div>
     Question {currentIndex + 1} of {questions.length}
   </div>
 
+{/*Quiz content */}
   {!completed ? (
     <div>
       <h2>{questions[currentIndex].question}</h2>
@@ -61,14 +92,26 @@ return(
   ) : (
     <div>
       <h2>Congratulations!</h2>
-      <p>You've completed the quiz.</p>
+      <p>You have completed the quiz successfully.</p>
     </div>
     
   )}
+
+    {/* Navigation Buttons */}
+    <div>
+        <button onClick={handlePrevious} disabled={currentIndex === 0}>
+          Previous
+        </button>
+
+        {!completed && (
+          <button onClick={handleNext}>
+            {currentIndex === questions.length - 1 ? "Finish" : "Next"}
+          </button>
+        )}
+      </div>
+    
   </div>
 );
-
-
-
+};
 
   export default QuizQuestion;
