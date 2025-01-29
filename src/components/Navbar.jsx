e663af728128d1dfc9e3f072c1dba56cc35aafd6
Natasha Nduku <natashanduku0@gmail.com>
2025-01-29 11:27:11 +0300

    final code for the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
     return(
         <nav>
           <ul>
-           <li></li>
-           <li></li>
-           <li></li>
-           <li></li>  
+           <li><Link to="/">Categories</Link></li>
+           <li><Link to="/quiz">Quiz</Link></li>
+           <li><Link to="/results">Results</Link></li>
+           <li><Link to="/summary">Summary</Link></li>  
           </ul>
         </nav>
       );
