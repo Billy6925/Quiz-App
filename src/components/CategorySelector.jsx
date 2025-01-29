87c351854d8dd023f135eb6e3e25aa0bd7b08836
Natasha Nduku <natashanduku0@gmail.com>
2025-01-29 12:14:20 +0300

    fetch categories and store them in categories

--- src/components/CategorySelector.jsx
+++ src/components/CategorySelector.jsx
@@ -3,7 +3,13 @@ import { useState } from 'react';
 export default function CategorySelector() { 
     const [categories, setCategories] = useState([]);
 
-    
+    useEffect(() => {
+        fetch('http://localhost:3001/categories')
+          .then(res => res.json())
+          .then(data => setCategories(data))
+          .catch(error => console.error('Error:', error));
+      }, []);
+      
     return (
       <div>
         <h2>Select a Category</h2>
