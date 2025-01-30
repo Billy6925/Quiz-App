e1403fa4e2ef13ace29ccbc3ec4eb20da2db8111
Billy Eskein <billyeskein@gmail.com>
2025-01-30 12:35:17 +0300

    modify src/components/QuizSummary.jsx

--- src/components/QuizSummary.jsx
+++ src/components/QuizSummary.jsx
@@ -1,13 +1,20 @@
 import React, {useState, useEffect} from "react";
+import NavBar from "./NavBar";
 
 function QuizSummary() {
     const [quizHistory, setQuizHistory] = useState([])
+    const [category, setCategory] = useState([])
 
     useEffect(()=> {
         fetch("http://localhost:3000/questions")
         .then(response => response.json())
         .then(data => setQuizHistory(data))
         .catch(error => console.error('Error fetching quiz history:',error))
+
+        fetch("http://localhost:3000/categories")
+        .then(response => response.json())
+        .then(data => setCategory(data))
+        .catch(error => console.error("Error fetching categories:", error))
     },[])
 
     return(
@@ -19,9 +26,9 @@ function QuizSummary() {
     <h2>Quiz Summary</h2>
     {quizHistory.map((quiz, index) => (
 <main key={index}>
-<h3>Quiz{quizHistory.length - index}</h3>
-<p>Categories:{quiz.categories.join(',')}</p>
-<p>Questions Completed{quiz.questionsCompleted}</p>
+<h3>Quiz {quizHistory.length - index}</h3>
+<p>Categories: {quiz.categories.join(', ')}</p>
+<p>Questions Completed {quiz.questionsCompleted}</p>
 <p>Correct Answers: {quiz.correctAnswers}</p>
 <ul>
     {
